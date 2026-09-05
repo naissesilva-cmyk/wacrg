6867c1c1c42aacaade6e0bb3875a3cb23be21a35
naissesilva-cmyk <naisse.silva@gmail.com>
2026-09-05 18:37:22 -0300

    fix(nora): accept canonical tenant UUID sentinels

--- nora-worker/src/index.ts
+++ nora-worker/src/index.ts
@@ -121,7 +121,7 @@ function loadConfig() {
   };
   const uuid = (name: string): string => {
     const value = required(name);
-    if (!/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value)) throw new Error(`Invalid ${name}`);
+    if (!/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(value)) throw new Error(`Invalid ${name}`);
     return value;
   };
   return {
